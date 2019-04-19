bfef8bd3bf941e68cb996f6d6737107c5ba36912
Jonathan Watson <jonathanw12@outlook.com>
2019-04-19 20:51:24 +0100

    Added a function to tokenize an expression

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const expression = document.getElementById('expression');
 
+const squeezeRegex = /\s+/g;
+const tokenRegex = /[A-Z]+|\d+|\W/gi;
+const tokenize = exp => exp.replace(squeezeRegex, '').match(tokenRegex);
+const strRegex = /[A-Z]+/i;
+
 const operator = (symbol) => {
   expression.value += symbol;
   expression.focus();
 };
+
+const newExpression = () => {
+  const tokens = tokenize(expression.value);
+  const variables = new Set(tokens.filter(token => strRegex.test(token)));
+};
+
+newExpression();
